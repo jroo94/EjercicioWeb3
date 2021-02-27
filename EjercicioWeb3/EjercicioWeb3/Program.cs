1735608b2515579782854272ec6a1d4c144aa62b
Ramon Osuna <ramonosuna72@gmail.com>
2021-02-26 20:49:19 -0700

    Ejercicio 3

---
+++ EjercicioWeb3/EjercicioWeb3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EjercicioWeb3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long result = 1;
+            
+            Console.WriteLine("Introduce un número");
+
+            var numero = Console.ReadLine();
+            var num = byte.Parse(numero);            
+
+            for (int i = 2; i <= num; i++)
+            {
+                result = result * i;
+            }
+
+            Console.WriteLine("El factorial de {0} es {1}", num, result);
+            Console.Write("Tecle para continuar");
+            Console.ReadLine();
+        }
+    }
+}
